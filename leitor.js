8a50b8b6d54fa8d492010a4d29d4e30213478d95
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2022-03-03 23:57:04 -0300

    :lipstick: fixing UI

---
+++ leitor.js
@@ -0,0 +1,16 @@
+let dados = []
+let contador = 0
+document.addEventListener("DOMContentLoaded", event => {
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+Instascan.Camera.getCameras().then(cameras => {
+scanner.camera = cameras[cameras.length - 1];
+scanner.start();
+}).catch(e => console.error(e));
+
+scanner.addListener('scan', content => {
+  dados.push(content)
+    contador++
+  document.querySelector('.changeHere').innerHTML = dados.toString()
+  document.querySelector('.number-count').innerHTML = contador + "itens"
+});
+});
